0f51538da0f76ff3b3d2508b3526c9cf4df550fa
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-21 07:18:49 -0400

    ex42

--- src/main/java/ex42/app.java
+++ src/main/java/ex42/app.java
@@ -5,9 +5,43 @@
 
 package ex42;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+import java.util.ArrayList;
+
 public class app {
 
-    public static void main(String[] args){
+    public void main(String[] args){
+        //Test for input file validity.
+        File inFile = new File("src/main/java/ex42/exercise42_input.txt");
 
+        Scanner readFile;
+        try {
+            readFile = new Scanner(inFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("exercise42_input.txt not found.");
+            return;
+        }
+
+        //Assign each line to object in arraylist.
+        ArrayList<empInfo> inputData = readData(readFile);
+
+        readFile.close();
+        //Output arraylist to console.
+
+    }
+
+    public ArrayList<empInfo> readData(Scanner readFile){
+        ArrayList<empInfo> inputData = new ArrayList<>();
+        empInfo newInfo = new empInfo();
+
+        while (readFile.hasNextLine()) {
+            newInfo.parseInfo(readFile.nextLine());
+            inputData.add(newInfo);
+        }
+
+        return inputData;
     }
 }
---
+++ src/main/java/ex42/empInfo.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+
+package ex42;
+
+public class empInfo {
+    private String fName, lName, salary;
+
+    //Set of methods to return information.
+    public String returnFName(){
+        return fName;
+    }
+    public String returnLName(){
+        return lName;
+    }
+    public String returnSalary(){
+        return salary;
+    }
+
+    public void parseInfo(String inData){
+        //Given string in format "fName,lName,salary", split into 3 strings
+        String[] splitData = inData.split(",");
+
+        //Assign each substring to attribute.
+        fName = splitData[0];
+        lName = splitData[1];
+        salary = splitData[2];
+    }
+}
